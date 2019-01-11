a5b386a03f0d4c315eef8d3309a7a8c57184785e
maximka777 <maksim.borovskij@yandex.ru>
2019-01-11 18:56:26 +0300

    Small imports fixes

--- demo/src/app/demo/demo.component.ts
+++ demo/src/app/demo/demo.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-import { AuthService } from '@maximka777/angularx-social-login';
-import { SocialUser } from '@maximka777/angularx-social-login';
-import { GoogleLoginProvider, FacebookLoginProvider, LinkedInLoginProvider } from '@maximka777/angularx-social-login';
+import { AuthService } from '@dffrntlab/angularx-social-login';
+import { SocialUser } from '@dffrntlab/angularx-social-login';
+import { GoogleLoginProvider, FacebookLoginProvider, LinkedInLoginProvider } from '@dffrntlab/angularx-social-login';
 
 
 @Component({
